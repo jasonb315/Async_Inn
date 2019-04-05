ba98eab12636e31507098fe240883b0380525653
Jason Burns <jasonb315@protonmail.com>
2019-04-04 21:09:57 -0700

    DB context partial

---
+++ AsyncInn/Data/AsyncInnDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AsyncInn.Models;
+
+namespace AsyncInn.Data
+{
+    public class AsyncInnDbContext : DBContext
+    {
+        public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HotelRooms>().HasKey(ce => new { ce.HotelsID, ce.RoomsID });
+            modelBuilder.Entity<RoomAmeneties>().HasKey(ce => new { ce.RoomsID, ce.AmmenitiesID });
+
+        }
+    }
+}
