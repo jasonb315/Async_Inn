7b60901f4af9f8a10bfe175aae5740469b288533
Jason Burns <jasonb315@protonmail.com>
2019-04-04 21:20:46 -0700

    hotel rooms class

--- AsyncInn/Models/HotelRooms.cs
+++ AsyncInn/Models/HotelRooms.cs
@@ -7,5 +7,13 @@ namespace AsyncInn.Models
 {
     public class HotelRooms
     {
+        public int HotelsID { get; set; }
+        public int RoomsID { get; set; }
+        public int RoomNumber { get; set; }
+        public decimal Rate { get; set; }
+        public bool PetFriendly { get; set; }
+
+        public Hotels Hotels { get; set; }
+        public Rooms Rooms { get; set; }
     }
 }
