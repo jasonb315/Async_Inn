7a31413fbc1e8985e00a2b9201fcc215a184940b
Jason Burns <jasonb315@protonmail.com>
2019-04-04 21:14:18 -0700

    db context

--- AsyncInn/Data/AsyncInnDbContext.cs
+++ AsyncInn/Data/AsyncInnDbContext.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AsyncInn.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AsyncInn.Data
 {
-    public class AsyncInnDbContext : DBContext
+    public class AsyncInnDbContext : DbContext
     {
         public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
         {
@@ -16,8 +17,13 @@ namespace AsyncInn.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<HotelRooms>().HasKey(ce => new { ce.HotelsID, ce.RoomsID });
-            modelBuilder.Entity<RoomAmeneties>().HasKey(ce => new { ce.RoomsID, ce.AmmenitiesID });
-
+            modelBuilder.Entity<RoomAmenities>().HasKey(ce => new { ce.RoomsID, ce.AmenitiesID });
         }
+
+        public DbSet<Hotels> Hotels { get; set; }
+        public DbSet<HotelRooms> HotelRooms { get; set; }
+        public DbSet<Rooms> Rooms { get; set; }
+        public DbSet<RoomAmenities> RoomAmenities { get; set; }
+        public DbSet<Amenities> Amenities { get; set; }
     }
 }
