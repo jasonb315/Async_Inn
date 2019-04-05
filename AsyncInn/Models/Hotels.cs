9c72cbb75f902ae3c8d9348ae6fa1579aa1d37a2
Jason Burns <jasonb315@protonmail.com>
2019-04-04 21:16:02 -0700

    Hotel class

--- AsyncInn/Models/Hotels.cs
+++ AsyncInn/Models/Hotels.cs
@@ -7,5 +7,13 @@ namespace AsyncInn.Models
 {
     public class Hotels
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
+
+        public ICollection<HotelRooms> HotelRooms { get; set; }
     }
 }
