cb6a3b0e003b7f23d464e12c05cbacc70389a22e
Jason Burns <jasonb315@protonmail.com>
2019-04-13 15:32:59 -0700

    hotels

--- AsyncInn/Models/Interfaces/IHotels.cs
+++ AsyncInn/Models/Interfaces/IHotels.cs
@@ -5,7 +5,22 @@ using System.Threading.Tasks;
 
 namespace AsyncInn.Models.Interfaces
 {
-    public class IHotels
+    public interface IHotels
     {
+        // C
+        Task CreateHotels(Hotels hotel);
+
+        // R
+        bool HotelsExists(int id);
+
+        Task<Hotels> GetHotel(int id);
+
+        Task<IEnumerable<Hotels>> GetHotels();
+
+        // U
+        Task UpdateHotels(int id, Hotels hotel);
+
+        // D
+        Task DeleteHotels(int id);
     }
 }
--- AsyncInn/Models/Services/HotelsServices.cs
+++ AsyncInn/Models/Services/HotelsServices.cs
@@ -1,4 +1,5 @@
-﻿using AsyncInn.Models.Interfaces;
+﻿using AsyncInn.Data;
+using AsyncInn.Models.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,5 +10,53 @@ namespace AsyncInn.Models.Services
 {
     public class HotelsServices : IHotels
     {
+        private AsyncInnDbContext _context;
+
+        public void AmenetieService(AsyncInnDbContext context)
+        {
+            _context = context;
+        }
+        // [C]RUD
+        // Create one
+        public async Task CreateHotels(Hotels hotel)
+        {
+            _context.Add(hotel);
+            await _context.SaveChangesAsync();
+        }
+        // C[R]UD
+        // Check one
+        public bool HotelsExists(int id)
+        {
+            return _context.Amenities.Any(x => x.ID == id);
+        }
+        // Read one
+        public async Task<Hotels> GetHotel(int id)
+        {
+            var hotel = await _context.Hotels.FindAsync(id);
+            if (hotel == null)
+            {
+                return null;
+            }
+            return hotel;
+        }
+        // Read all
+        public async Task<IEnumerable<Hotels>> GetHotels()
+        {
+            return await _context.Hotels.ToListAsync();
+        }
+
+        // CR[U]D
+        public async Task UpdateHotels(int id, Hotels hotel)
+        {
+            _context.Update(hotel);
+            await _context.SaveChangesAsync();
+        }
+        // CRU[D]
+        public async Task DeleteHotels(int id)
+        {
+            Hotels hotel = await _context.Hotels.FindAsync(id);
+            _context.Hotels.Remove(hotel);
+            await _context.SaveChangesAsync();
+        }
     }
 }
